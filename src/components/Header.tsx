d2271b14a8cf63763957ff5f0654746c349c235c
Omar Abdallah <oamas857@gmail.com>
2025-09-17 01:56:28 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import type { ITodo } from "../App"
 
 export function Header({todos}: Readonly<{todos: ITodo[]}>){
-    const OpenTasks = todos.filter(val=> !val.complete).length
+    const OpenTasks = todos.filter(val=> !val?.complete).length
     return(
         <header>
             <h1 className="text-gradient">You have {(OpenTasks ==1? "1 open task.":`${OpenTasks} open tasks.`)} </h1>
